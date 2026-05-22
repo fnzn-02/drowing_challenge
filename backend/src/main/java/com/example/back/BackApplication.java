b90b021573b254e821427ff617bb85ceeea7f49b
Seok_JH <junho5550@naver.com>
2026-05-22 16:03:51 +0900

    업데이트

--- backend/src/main/java/com/example/back/BackApplication.java
+++ backend/src/main/java/com/example/back/BackApplication.java
@@ -1,13 +1,16 @@
+
 package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackApplication.class, args);
 	}
 
 }
+

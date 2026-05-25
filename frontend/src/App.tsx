4007a1646bb8276c64b7b2c3dcf38bbaa5a3bce5
Seok_JH <junho5550@naver.com>
2026-05-25 19:37:57 +0900

    그림판 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-=======
+import Drawing from './components/Drawing'
 
->>>>>>> acca2187885ee9c5abcfff2443e6cf4b8abcc9b8
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Drawing />
   )
 }
 
